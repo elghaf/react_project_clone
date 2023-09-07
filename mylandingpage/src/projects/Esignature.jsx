c39abc69b5d8b1658bf1ecdfe8a44411bef315d2
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-07 22:20:21 +0100

    Update Esignature.jsx

--- mylandingpage/src/projects/Esignature.jsx
+++ mylandingpage/src/projects/Esignature.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Title from '../components/Title'
 
 function Esignature() {
-  return (
+const inputStyle= {
+    border:"none",
+    borderBottom: "2px dotted",
+    outline: "none",
+    padding: ".35rem 0",
+};
+document.body.style.background = "#eee";
+
+const [name, setName] = useState("noble");
+
+const handlerNameChange = (e) =>{
+setName(e.target.value)
+}
+return (
     <div className='container text-center'>
-        <Title classes={"title"} text="Name"/>
+        <Title classes={"title"} text={name}/>
         <Title classes={"main-title"} text="Date"/>
+        <p>
+            ayoub is one of the best programmer in the world inchaallah,
+            ila bra allah
+            </p>
+    <footer className='d-flex' style={{
+        justifyContent: "space-around",
+        position:"relative",
+        top:"70vh"
+    }}>
+         <input type='date' value={""} style={inputStyle}/>
+         <input type='text' value={name} style={inputStyle} onChange={handlerNameChange}/>
+    </footer>
     </div>
   )
 }
