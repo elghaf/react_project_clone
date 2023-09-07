db480df9efdfefb052a8513cd9b6a0f83a6dad48
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-07 21:34:51 +0100

    add

---
+++ mylandingpage/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Esignature from './projects/Esignature';
+
+function App() {
+  return (
+    <div className="App">
+     <Esignature/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mylandingpage/src/projects/Esignature.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../components/Title'
+
+function Esignature() {
+  return (
+    <div className='container text-center'>
+        <Title classes={"title"} text="Name"/>
+        <Title classes={"main-title"} text="Date"/>
+    </div>
+  )
+}
+
+export default Esignature
